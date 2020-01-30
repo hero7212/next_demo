02958d2b7b350be746e46fe0cdd7eb57608fe572
frontendgod <3054750147@qq.com>
2020-01-30 15:45:27 +0800

    p11 done

--- pages/header.js
+++ pages/header.js
@@ -1,10 +1,15 @@
 import Myheader from '../components/myheader'
+import '../static/test.css'
+import {Button} from 'antd'
+
+
 
 function Header() {
     return (
         <div>
             <Myheader></Myheader>
             <div>JSPang.com</div>
+            <div><Button>按钮</Button></div>
         </div>
     )
 }
