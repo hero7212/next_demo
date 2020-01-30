f89235312c7c773fec6f364346ee9b5c405d5d47
frontendgod <3054750147@qq.com>
2020-01-30 14:46:21 +0800

    p10

---
+++ components/myheader.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const MyHeader = () => {
+    return (
+        <div>
+            <Head>
+                <title>God</title>
+            </Head>
+        </div>
+    )
+}
+
+export default MyHeader
---
+++ pages/header.js
@@ -0,0 +1,11 @@
+import Myheader from '../components/myheader'
+
+function Header() {
+    return (
+        <div>
+            <Myheader></Myheader>
+            <div>JSPang.com</div>
+        </div>
+    )
+}
+export default Header
